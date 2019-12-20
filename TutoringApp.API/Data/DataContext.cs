aaa674c7f67f6351947bf890185de91face39c7f
Kartik Buddha <kartik14@gmail.com>
2019-12-20 21:50:11 +0100

    Walking Skeleton
    ASP.NET Backend
    Angular Frontend

---
+++ TutoringApp.API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TutoringApp.API.Data
+{
+	public class DataContext : DbContext
+	{
+		public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+		public DbSet<Models.Value> Values { get; set; }
+	}
+}
